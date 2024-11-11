16ff5af55906ab260a2741af5f026bf3a5c6b636
sanha <heftycornerstone@gmail.com>
2024-11-11 19:35:46 +0900

    modified : props-drilling branch is refactored

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter} from "react-router-dom";
 import Home from "../pages/Home";
 import Dex from "../pages/Dex";
 import Details from "../pages/Details";
 
-const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/dex" element={<Dex />} />
-                <Route path="/details/:pokemonId" element={<Details />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/dex',
+        element: <Dex />
+    },
+    {
+        path: '/details/:pokemonId',
+        element: <Details />
+    },
+])
 
-export default Router;
+export default router;
