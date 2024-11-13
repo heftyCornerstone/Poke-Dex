b023db8cd25ceb1f97b7823c575b0e1dab3d17d6
sanha <heftycornerstone@gmail.com>
2024-11-13 15:30:22 +0900

    modified : useless lines are deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import store from "./redux/config/configStore";
-import { Provider } from "react-redux";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </StrictMode>,
 )
